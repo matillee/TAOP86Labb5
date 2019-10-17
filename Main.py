dae26e4a24eb83dae02d69ec927c4861f720ee0b
Matilda <mater832@student.liu.se>
2019-10-17 11:52:41 +0200

    ett steg på vägen

--- Main.py
+++ Main.py
@@ -11,14 +11,20 @@ e=1 #diskonteringsfaktor
 prob=" ".join(sys.argv[1:]).split('.')[0]
 fil=prob+'.npz'
 
-npzfile = np.load("extrdata/floc3.npz") #den kan ladda!
+npzfile = np.load("extrdata/floc1.npz") #den kan ladda!
 npzfile.files
-m=npzfile['m'] #antal mojliga platser for lokalisering av anlaggning/fabrik
-n=npzfile['n'] #antal kunder
-s=npzfile['s'] #kapacitet hos anlaggning i
-d=npzfile['d'] #efterfragan hos kund j
-f=npzfile['f'] #fasta kostnader att oppna anlaggning pa plats i
-c=npzfile['c'] #transportkostnad per enhet till kund j fran plats i
+m=npzfile['m'] #antal mojliga platser for lokalisering av anlaggning/fabrik. skalärer
+n=npzfile['n'] #antal kunder. skalärer
+
+s=npzfile['s'] #kapacitet hos anlaggning i. kolumnvektor
+f=npzfile['f'] #fasta kostnader att oppna anlaggning pa plats i. kolumnvektor
+
+c=npzfile['c'] #transportkostnad per enhet till kund j fran plats i. array 2D
+#rad i c motsvarar kund. kolumn i c motsvarar anlaggning
+#c.item(anlaggning, kund) for att fa transportkostnad ish
+
+d=npzfile['d'] #efterfragan hos kund j. kolumnvektor
+
 #print 'm:',m,' n:',n
 #print 's:',s
 #print 'd:',d
@@ -29,29 +35,22 @@ t1=time.time()
 x=np.zeros((m,n),dtype=np.int)
 y=np.zeros((m),dtype=np.int)
 
+#yi = 1 om en anlaggning finns på plats i, 0 annars
+
+#xij = transporterad mangd fran anlaggning pa plats i till kund j
+
 ss=copy.deepcopy(s)
+cc=copy.deepcopy(c)
 dd=copy.deepcopy(d)
 ff=copy.deepcopy(f)
 
     #Vi bestammer att vi vill hitta den dyraste fabriken forst
     #Vi vill ocksa ta den kund med hogst efterfragan
 
-    #Vi vill alltsa sortera dd och ff sa storsta elementet ar forst.
-
-sorted(dd, reverse=True)
-sorted(ff, reverse=True)
-
-
 print(dd)
 print(ff)
 
-# sedan vill hitta samma element i riktiga d i och f. Hitta index for dessa.
-
-storst_kund = d.index(dd[0])
-dyrast_fabrik = f.index(ff[0])
-
-print(storst_kund)
-print(dyrast_fabrik)
+total_kostnad = 0
 
 #while sum(dd)>0:
     # find facility, find customer, send, at min cost
@@ -59,7 +58,31 @@ print(dyrast_fabrik)
     # deduct from ss and dd,
     # --------
 
+storst_kund = dd.max()
+index_kund = dd.argmax()
 
+dyrast_fabrik = ff.max()
+index_fabrik = ff.argmax()
+
+
+
+print(storst_kund)
+print(index_kund)
+print(dyrast_fabrik)
+print(index_fabrik)
+
+transport_kostnad = cc.item(index_fabrik, index_kund)
+fast_kostnad = ff.item(index_fabrik)
+
+total_kostnad = transport_kostnad + fast_kostnad
+
+print(transport_kostnad)
+print(fast_kostnad)
+
+transport_kostnad = 0
+fast_kostnad = 0
+ff = np.delete(ff, index_fabrik)
+print(ff)
 
     #
 
@@ -67,9 +90,9 @@ print(dyrast_fabrik)
 
 
 elapsed = time.time() - t1
-print 'Tid: '+str('%.4f' % elapsed)
+print('Tid: '+ str('%.4f' % elapsed))
 
 cost=sum(sum(np.multiply(c,x))) + e*np.dot(f,y)
-print 'Problem:',prob,' Totalkostnad: '+str(cost)
-print 'y:',y
-print 'Antal byggda fabriker:',sum(y),'(av',m,')'
+print( 'Problem:',prob,' Totalkostnad: '+str(cost))
+print( 'y:',y)
+print('Antal byggda fabriker:',sum(y),'(av',m,')')
